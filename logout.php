c659fdd3d521aacfbb7c1199612b6267f68feecd
XNM1 <artemx8zx@gmail.com>
2021-11-28 18:34:49 +0200

    logout redirect update

--- logout.php
+++ logout.php
@@ -2,9 +2,8 @@
     session_start();
     if (isset($_SESSION['ACCESS_TOKEN'])) {
         session_destroy();
-
-        $host = $_SERVER['SERVER_NAME'];
-        header("Location: https://$host/login.php");
-        exit();
     }
+    $host = $_SERVER['SERVER_NAME'];
+    header("Location: https://$host/login.php");
+    exit();
 ?>
